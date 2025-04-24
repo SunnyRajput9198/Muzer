12a93ee479bfe2976c03be5e3b9d043452808abc
sunny <rajputsny2@gmail.com>
2025-04-25 02:34:00 +0530

    majorly fixed the spaceid issue

--- muzer/app/api/auth/[...nextauth]/route.ts
+++ muzer/app/api/auth/[...nextauth]/route.ts
@@ -1,35 +1,7 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import { PrismaClient } from "@prisma/client"
-import prisma from "@/lib/db"
+import NextAuth from "next-auth";
 
-const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-          clientId: process.env.GOOGLE_CLIENT_ID??"",// assigned empty string to prevent build error
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET??"",
-        })
-      ],
-      secret:process.env.NEXTAUTH_SECRET??"",
-      callbacks: {
-        async signIn(params){
-          if(!params.user.email){
-            return false
-          }
-          try{
-            await prisma.user.create({
-              data:{
-                email:params.user.email??"",
-                provider:"Google"
-              }
-            })
-            return true
-          }catch(e){
-            return false
+import { authOptions } from "@/lib/auth-options";
 
-          }
-          return true
-        }
-}})
+const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
--- muzer/app/home/page.tsx
+++ muzer/app/home/page.tsx
@@ -1,14 +1,15 @@
-import React from "react";
+import HomeView from "@/components/HomeView";
+import { authOptions } from "@/lib/auth-options";
+import { getServerSession } from "next-auth";
 
-export default function LoadingScreen() {
-  return (
-    <div className=" bg-[rgb(10,10,10)] text-gray-200">
-      <div className="flex space-x-2 justify-center items-center w-screen h-screen dark:invert">
-        <span className="sr-only">Loading...</span>
-        <div className="h-8 w-8 bg-white rounded-full animate-bounce [animation-delay:-0.3s]"></div>
-        <div className="h-8 w-8 bg-white rounded-full animate-bounce [animation-delay:-0.15s]"></div>
-        <div className="h-8 w-8 bg-white rounded-full animate-bounce"></div>
-      </div>
-    </div>
-  );
+
+
+export default async function Home(){
+  const session =await  getServerSession(authOptions);
+
+  if (!session?.user.id) {
+    return <h1>Please Log in....</h1>;
+  }
+ return <HomeView></HomeView>
+
 }
