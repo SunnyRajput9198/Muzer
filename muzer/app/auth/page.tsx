59778b71aa01bc6790818952ff86a7463d35eddd
sunny <rajputsny2@gmail.com>
2025-05-06 04:20:33 +0530

    fixed all build issues

--- muzer/app/auth/page.tsx
+++ muzer/app/auth/page.tsx
@@ -1,25 +1,10 @@
-'use client';
-
-import { useSession } from "next-auth/react";
-import { useRouter, useSearchParams } from "next/navigation"; // Import useSearchParams
-import { useEffect } from "react"; // Import useEffect
-
-import { SignInFlow } from "@/types/auth-types";
-import AuthScreen from "@/components/auth/authscreen";
+import { Suspense } from "react";
+import AuthClient from "./AuthClient"; // Adjust path if needed
 
 export default function AuthPage() {
-  const searchParams = useSearchParams(); // Get searchParams dynamically
-  const formType = searchParams?.get("authType") as SignInFlow; // Access authType safely
-  const session = useSession();
-  const router = useRouter();
-
-  // Use useEffect to handle the redirection
-  useEffect(() => {
-    if (session.status === "authenticated") {
-      router.push("/");
-    }
-  }, [session.status, router]); // Depend on session status and router
-
-  // Render the AuthScreen if the user is not authenticated
-  return <AuthScreen authType={formType} />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <AuthClient />
+    </Suspense>
+  );
 }
