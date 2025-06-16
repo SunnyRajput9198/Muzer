a817424efe5d1e6b99d86722b7fc18b11a443f1f
sunny <rajputsny2@gmail.com>
2025-06-16 19:57:57 +0530

    changes-yt-regex

--- ws/src/utils.ts
+++ ws/src/utils.ts
@@ -1,7 +1,7 @@
 import { WebSocket } from "ws";
 
 const YT_REGEX =
-  /^(?:https?:\/\/)?(?:www\.)?(?:m\.)?(?:youtube\.com\/(?:watch\?(?!.*\blist=)(?:.*&)?v=|embed\/|v\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})(?:[?&]\S+)?$/;
+ /^(?:https?:\/\/)?(?:www\.)?(?:m\.)?youtube\.com\/watch\?(?:.*&)?v=([a-zA-Z0-9_-]{11})(?:&.*)?(?:list=([a-zA-Z0-9_-]+))?/;
 
 export const isValidYoutubeURL = (data: string) => {
   return data.match(YT_REGEX);
